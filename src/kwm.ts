9e3eba9532432eee3e5d9d669f907d30aba70409
Simon Eisenmann <s.eisenmann@kopano.com>
2017-09-11 15:22:27 +0200

    Change project license to MIT license
    References: KWM-24, KWM-27, KWM-30

--- src/kwm.ts
+++ src/kwm.ts
@@ -1,18 +1,12 @@
 /*!
- * Copyright 2017 Kopano and its licensors
+ * Copyright 2017 Kopano
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
+ * Use of this source code is governed by a MIT license
+ * that can be found in the LICENSE.txt file.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * @author   Kopano <https://kopano.com>
+ * @license  MIT
+ * @preserve
  */
 
 'use strict';

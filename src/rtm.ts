665b357232b202b5cf596555efc70bbcbeef9635
Simon Eisenmann <s.eisenmann@kopano.com>
2017-09-14 18:13:59 +0200

    Add admin API and authentication
    Tokens are used for authentication to the RESTful API of KWM. Tokens can
    be created/added using the new /api/v1/admin/auth/tokens endpoint. Token
    values are self contained digitally signed JSON Web Tokens and can also
    be used to resurrect authenticated sessions. This change adds the
    implementation, endpoints and data structure and makes KWM usable
    securely in a public environment.

--- src/rtm.ts
+++ src/rtm.ts
@@ -38,6 +38,7 @@ export interface IRTMTypeSubTypeEnvelope extends IRTMTypeEnvelope {
 
 export interface IRTMTypePingPong extends IRTMTypeEnvelope {
 	ts: number;
+	auth?: string;
 }
 
 export interface IRTMTypeWebRTC extends IRTMTypeSubTypeEnvelope {
